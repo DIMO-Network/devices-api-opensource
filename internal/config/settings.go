cdfba38ab8a259fa71060dff38b2a00314806650
Yevgeny Khessin <sqr@Yevgenys-MacBook-Pro.local>
2021-10-23 10:30:18 -0400

    Initial commit for application

---
+++ internal/config/settings.go
@@ -0,0 +1,10 @@
+package config
+
+type Settings struct {
+	port       string
+	logLevel   string
+	dbUser     string
+	dbPassword string
+	dbPort     string
+	dbHost     string
+}
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/rs/zerolog/log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Use(recover.New())
+	app.Use(cors.New())
+
+	// Routes
+	app.Get("/", HealthCheck)
+
+	// Start Server
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// HealthCheck godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router / [get]
+func HealthCheck(c *fiber.Ctx) error {
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
+
+	if err := c.JSON(res); err != nil {
+		return err
+	}
+
+	return nil
+}
