0abb6b9dd2e8152edb0778afe1f45ea6b2dbd885
James Reategui <jreate@me.com>
2021-12-02 12:49:25 -0500

    use command args for migrations

--- cmd/devices-api/migrate.go
+++ cmd/devices-api/migrate.go
@@ -2,24 +2,16 @@ package main
 
 import (
 	"database/sql"
-	"log"
-
 	"github.com/DIMO-INC/devices-api/internal/config"
 	"github.com/pressly/goose/v3"
-
-	_ "github.com/DIMO-INC/devices-api/migrations"
-	_ "github.com/lib/pq"
+	"github.com/rs/zerolog"
+	"log"
 )
 
-func main() {
-	// would doing all this just be same as `goose up` and setting env vars GOOSE_DRIVER=postgres and GOOSE_DBSTRING=DSN ?
-	settings, err := config.LoadConfig("settings.yaml")
-	if err != nil {
-		log.Fatalf("failed to load settings: %v\n", err)
-	}
+func migrateDatabase(logger zerolog.Logger, settings *config.Settings) {
 	var db *sql.DB
 	// setup database
-	db, err = sql.Open("postgres", settings.GetWriterDSN(false))
+	db, err := sql.Open("postgres", settings.GetWriterDSN(false))
 	defer func() {
 		if err := db.Close(); err != nil {
 			log.Fatalf("goose: failed to close DB: %v\n", err)
