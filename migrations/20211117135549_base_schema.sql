b6e53cb026cdd7c20fb59d3fff157050ad35d93c
Yevgeny Khessin <sqr@Yevgenys-MacBook-Pro.local>
2021-12-02 19:49:54 -0500

    Updating schema migration files

--- migrations/20211117135549_base_schema.sql
+++ migrations/20211117135549_base_schema.sql
@@ -1,7 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
 SELECT 'up SQL query';
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public; -- uuid gen
 REVOKE CREATE ON schema public FROM public; -- public schema isolation
 CREATE SCHEMA IF NOT EXISTS devices_api;
 
@@ -13,5 +12,4 @@ SELECT 'down SQL query';
 DROP TABLE devices_api.migrations;
 DROP SCHEMA devices_api CASCADE;
 GRANT CREATE, USAGE ON schema public TO public;
-DROP EXTENSION IF EXISTS "uuid-ossp";
 -- +goose StatementEnd
