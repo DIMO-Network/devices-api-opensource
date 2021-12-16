457994460fa265f6bbea367161f9a1308d9b82b7
James Reategui <jreate@me.com>
2021-12-16 08:23:20 -0500

    csv loader fix, wait for db

--- cmd/devices-api/load_mmy_csv.go
+++ cmd/devices-api/load_mmy_csv.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	_ "embed"
 	"encoding/csv"
 	"fmt"
@@ -16,15 +17,16 @@ import (
 	"io"
 	"strconv"
 	"strings"
+	"time"
 )
 
 //go:embed mmy_definitions.csv
 var mmyDefinitions string
 
 func loadMMYCSVData(ctx context.Context, logger zerolog.Logger, settings *config.Settings, pdb database.DbStore) {
-	// csv loader
-	// for each
-	// check for duplicate in DB, insert, if duplicate log
+	// check db ready
+	time.Sleep(time.Second * 3)
+
 	csvReader := csv.NewReader(strings.NewReader(mmyDefinitions))
 	for {
 		rec, err := csvReader.Read()
@@ -38,22 +40,25 @@ func loadMMYCSVData(ctx context.Context, logger zerolog.Logger, settings *config
 		fmt.Printf("%+v\n", rec)
 		yrInt, err := strconv.Atoi(rec[2])
 		if err != nil {
-			logger.Fatal().Err(err).Msg("can't parse year")
+			logger.Info().Err(err).Msg("can't parse year: " + rec[2])
+			continue
 		}
 		dd, err := models.DeviceDefinitions(
 			qm.Where("make = ?", strings.ToUpper(rec[0])),
 			qm.And("model = ?", strings.ToUpper(rec[1])),
 			qm.And("year = ?", yrInt)).One(ctx, pdb.DBS().Writer)
-		if err != nil {
+		if err != nil && err != sql.ErrNoRows {
 			logger.Fatal().Err(err).Msg("can't read existing definition")
 		}
 		if dd != nil {
+			fmt.Printf(" ignoring, already exists: %s", dd.ID)
 			continue
 		}
+
 		dd = &models.DeviceDefinition{
 			ID:       ksuid.New().String(),
-			Make:     rec[0],
-			Model:    rec[1],
+			Make:     strings.ToUpper(rec[0]),
+			Model:    strings.ToUpper(rec[1]),
 			Year:     int16(yrInt),
 			Source:   null.StringFrom("csv import"),
 			Verified: true,
