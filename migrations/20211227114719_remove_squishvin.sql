ad04959ea34a4aaef73a1d96389063f5df4aa80d
James Reategui <jreate@me.com>
2021-12-27 12:09:07 -0500

    fix down migration

--- migrations/20211227114719_remove_squishvin.sql
+++ migrations/20211227114719_remove_squishvin.sql
@@ -10,4 +10,5 @@ ALTER TABLE device_definitions DROP COLUMN vin_first_10;
 SELECT 'down SQL query';
 SET search_path = devices_api, public;
 ALTER TABLE device_definitions ADD COLUMN vin_first_10 varchar(10);
+CREATE UNIQUE INDEX vin_idx ON device_definitions (vin_first_10);
 -- +goose StatementEnd
