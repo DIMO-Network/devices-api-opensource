71bd238ffbee2840dbfcbd50b24450fadd4e968b
Dylan Moreland <dylan@dimo.zone>
2022-03-01 17:40:29 -0500

    A little more logging

--- cmd/devices-api/migrate_smartcar_webhooks.go
+++ cmd/devices-api/migrate_smartcar_webhooks.go
@@ -48,15 +48,32 @@ func migrateSmartcarWebhooks(ctx context.Context, logger *zerolog.Logger, settin
 		return fmt.Errorf("failed to retrieve all API integrations with external IDs and status Active: %w", err)
 	}
 
+	success := 0
+	fail := 0
+
 	// For each of these send a new registration message, keyed by Smartcar vehicle ID.
 	for _, apiInt := range apiInts {
+		good := true
 		vehicleID := apiInt.ExternalID.String
 		if err := oldClient.Unsubscribe(vehicleID, apiInt.AccessToken); err != nil {
 			logger.Err(err).Msgf("Failed to unsubscribe %s from the old webhook", vehicleID)
+		} else {
+			logger.Info().Msgf("Successfully unsubscribed %s from old webhook", vehicleID)
 		}
 		if err := newClient.Subscribe(vehicleID, apiInt.AccessToken); err != nil {
 			logger.Err(err).Msgf("Failed to subscribe %s to the new webhook", vehicleID)
+			good = false
+		} else {
+			logger.Info().Msgf("Successfully subscribed %s to new webhook", vehicleID)
 		}
+
+		if good {
+			success += 1
+		} else {
+			fail += 1
+		}
+
+		logger.Info().Msgf("Succeed: %d, Fail: %d", success, fail)
 	}
 
 	return nil
