a6a8cdbb952910fbdd36774e90bef09e9319681f
James Reategui <jreate@me.com>
2022-03-08 21:47:32 -0500

    migration drop constraint if exists as doesn't in dev

--- migrations/20220304113644_makes_constraints.sql
+++ migrations/20220304113644_makes_constraints.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 SET search_path TO devices_api, public;
-ALTER TABLE device_definitions DROP CONSTRAINT device_definitions_make_model_year_key;
+ALTER TABLE device_definitions DROP CONSTRAINT IF EXISTS device_definitions_make_model_year_key;
 ALTER TABLE device_definitions ALTER COLUMN device_make_id SET NOT NULL;
 ALTER TABLE device_definitions ALTER COLUMN make DROP NOT NULL;
 ALTER TABLE device_definitions ADD CONSTRAINT idx_device_make_id_model_year UNIQUE (device_make_id, model, year);
