f8a43248df2e212e01d595dbd614c497df1896bb
Dylan Moreland <dylan@dimo.zone>
2022-04-22 15:58:05 -0400

    Need to generate a new ID

--- cmd/devices-api/restart_tesla_tasks.go
+++ cmd/devices-api/restart_tesla_tasks.go
@@ -10,6 +10,9 @@ import (
 	"github.com/DIMO-Network/devices-api/models"
 	"github.com/DIMO-Network/shared"
 	"github.com/rs/zerolog"
+	"github.com/segmentio/ksuid"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func restartTeslaTasks(ctx context.Context, logger *zerolog.Logger, settings *config.Settings, pdb database.DbStore, teslaSvc services.TeslaService, teslaTask services.TeslaTaskService, cipher shared.Cipher) error {
@@ -54,6 +57,13 @@ func restartTeslaTasks(ctx context.Context, logger *zerolog.Logger, settings *co
 			logger.Err(err).Msg("Failed to stop old task.")
 			continue
 		}
+
+		integ.TaskID = null.StringFrom(ksuid.New().String())
+		if _, err := integ.Update(ctx, pdb.DBS().Writer, boil.Infer()); err != nil {
+			logger.Err(err).Msg("Failed to update row.")
+			continue
+		}
+
 		if err := teslaTask.StartPoll(v, integ); err != nil {
 			logger.Err(err).Msg("Failed to start new task.")
 			continue
