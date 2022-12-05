60f2933fb65d1dfb493e6b1bf8fc65b0463effe0
Dylan Moreland <79415431+elffjs@users.noreply.github.com>
2022-12-05 15:51:39 -0500

    List user's devices: serial, user id, owner address (#342)

--- internal/api/aftermarket_devices.go
+++ internal/api/aftermarket_devices.go
@@ -2,14 +2,12 @@ package api
 
 import (
 	"context"
-	"database/sql"
-	"errors"
 
 	"github.com/DIMO-Network/devices-api/internal/database"
 	"github.com/DIMO-Network/devices-api/models"
 	pb "github.com/DIMO-Network/shared/api/devices"
 	"github.com/rs/zerolog"
-	"github.com/volatiletech/sqlboiler/v4/types"
+	"github.com/volatiletech/null/v8"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -24,38 +22,27 @@ type aftermarketDeviceService struct {
 	logger *zerolog.Logger
 }
 
-func (s *aftermarketDeviceService) GetDeviceBySerial(ctx context.Context, req *pb.GetDeviceBySerialRequest) (*pb.AftermarketDevice, error) {
-	unit, err := models.AutopiUnits(
-		models.AutopiUnitWhere.AutopiUnitID.EQ(req.Serial),
-	).One(ctx, s.dbs().Reader)
+func (s *aftermarketDeviceService) ListAftermarketDevicesForUser(ctx context.Context, req *pb.ListAftermarketDevicesForUserRequest) (*pb.ListAftermarketDevicesForUserResponse, error) {
+	units, err := models.AutopiUnits(
+		models.AutopiUnitWhere.UserID.EQ(null.StringFrom(req.UserId)),
+	).All(ctx, s.dbs().Reader)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return nil, status.Error(codes.NotFound, "No device with that id found.")
-		}
+		s.logger.Err(err).Str("userId", req.UserId).Str("method", "ListAftermarketDevicesForUser").Msg("Database failure.")
 		return nil, status.Error(codes.Internal, "Internal error.")
 	}
 
-	out := pb.AftermarketDevice{
-		Serial:         req.Serial,
-		VehicleTokenId: s.toUint64(unit.VehicleTokenID),
+	out := make([]*pb.AftermarketDevice, len(units))
+
+	for i, unit := range units {
+		out[i] = &pb.AftermarketDevice{
+			Serial: unit.AutopiUnitID,
+			UserId: &req.UserId,
+		}
+
+		if unit.OwnerAddress.Valid {
+			out[i].OwnerAddress = unit.OwnerAddress.Bytes
+		}
 	}
 
-	return &out, nil
-}
-
-// toUint64 takes a nullable decimal and returns nil if there is no value, or
-// a reference to the uint64 value of the decimal otherwise. If the value does not
-// fit then we return nil and log.
-func (s *aftermarketDeviceService) toUint64(dec types.NullDecimal) *uint64 {
-	if dec.IsZero() {
-		return nil
-	}
-
-	ui, ok := dec.Uint64()
-	if !ok {
-		s.logger.Error().Str("decimal", dec.String()).Msg("Value too large for uint64.")
-		return nil
-	}
-
-	return &ui
+	return &pb.ListAftermarketDevicesForUserResponse{AftermarketDevices: out}, nil
 }
